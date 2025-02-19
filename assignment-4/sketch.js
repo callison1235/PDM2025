c585ca4ed7f1d92eabddffd9c1dc67ae5b459b9a
RoscoMan123 <124205084+RoscoMan123@users.noreply.github.com>
2025-02-18 20:29:27 -0600

    setup assignment4

--- assignment-4/sketch.js
+++ assignment-4/sketch.js
@@ -1,7 +1,45 @@
+let character;
+let bug;
+
+function preload(){
+  bug = loadImage('media/bug.png');
+}
+
 function setup() {
-  createCanvas(400, 400);
+  createCanvas(600, 600);
 }
 
 function draw() {
-  background(220);
+  background('lightblue');
+}
+
+class Character{
+  constructor(x,y){
+    this.x = x;
+    this.y = y;
+    this.currentAnimation = null;
+    this.animations = {};
+  }
+
+  addAnimation(key, animation){
+    this.animations[key] = animation;
+  }
+
+  draw(){
+    let animation = this.animations[this.currentAnimation];
+    if (animation){
+      switch(this.currentAnimation){
+        case "up":
+          this.y += 2;
+          break
+        case "down":
+          this.y -= 2;
+          break
+      }
+      push();
+      translate(this.x, this.y);
+      animation.draw();
+      pop();
+    }
+  }
 }
